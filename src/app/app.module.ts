c80de9867630287269f113e2e371953c54918f72
bingnanzhou <bingnan.zhou@weather.com>
2020-10-15 15:43:15 -0400

    feat(tutorial): complete step 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+// carbon-components-angular default imports
+import { UIShellModule } from 'carbon-components-angular';
+import { NotificationModule } from '@carbon/icons-angular/';
+import { UserAvatarModule } from '@carbon/icons-angular/';
+import { AppSwitcherModule } from '@carbon/icons-angular/';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		UIShellModule,
+		NotificationModule,
+		UserAvatarModule,
+		AppSwitcherModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
